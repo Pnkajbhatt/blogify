963146955245396caa6f9fa1f084c92c28663d6f
PANKAJ BHATT <211pankajbhatt@gmail.com>
2025-11-05 22:32:07 +0530

    server start

--- server/src/index.js
+++ server/src/index.js
@@ -4,14 +4,21 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
 const app = express();
+const connectDB = require("./config/db.js");
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Mongo connected"))
-  .catch((err) => console.error(err));
-app.get("/", (req, res) => res.send("Blogify API"));
+
+connectDB();
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend is LIVE!" });
+});
+
+app.post("/api/data", (req, res) => {
+  res.json({ message: "API working!" });
+});
 app.listen(process.env.PORT, () =>
   console.log(`Server on ${process.env.PORT}`)
 );
